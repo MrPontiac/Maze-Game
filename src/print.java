6166aca21080058ad745dc26bfe56ad3f1179dd0
mrpontiac527 <mrpontiac527@gmail.com>
2019-02-11 15:40:36 -0500

    Format

---
+++ src/print.java
@@ -0,0 +1,10 @@
+class print {
+	public static void Maze(char[][] grid, int c, int r) {
+		for (int i = 0; i < c; i++) {
+			for (int j = 0; j < r; j++) {
+				System.out.print(grid[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
